e968e8957b2c2ec425eea8600a7f07c9effd2817
WearWellWardrobe <jh03.project.wearwellwardrobe@gmail.com>
2025-03-05 15:14:16 +0000

    Add files via upload

--- projectSite/wsgi.py
+++ projectSite/wsgi.py
@@ -1,15 +1,16 @@
+"""
+WSGI config for projectSite project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
 import os
-import sys
 
-
-sys.path.append('/home/WearWellWardrobe/WearWellWardrobe')
-sys.path.append('/home/WearWellWardrobe/WearWellWardrobe/projectSite')
-
-
-activate_this = '/home/WearWellWardrobe/.virtualenvs/WearWellWardrobe/bin/activate_this.py'
-exec(open(activate_this).read(), dict(__file__=activate_this))
+from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projectSite.settings')
 
-from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
